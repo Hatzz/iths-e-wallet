bfc882ba31f6cd15738b53c6de943099e1706272
Hatzz <git@hatzz.io>
2021-01-26 18:15:47 +0100

    Add active card and stack cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
   <main>
-    <h1>Hello world</h1>
-    <Card
+    <h1>E-WALLET</h1>
+    <h2>ACTIVE CARD</h2>
+    <Card class="active-card" :card="$root.cards[0]"/>
+    <div class="cards">
+      <Card
       v-for="card in this.$root.cards"
       :card="card"
       :key="card.number"
     />
+    </div>
   </main>
 </template>
 
@@ -21,4 +25,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  .active-card {
+    margin-bottom: 2rem;
+  }
+  .cards {
+    display: grid;
+    grid-auto-rows: 4rem;
+  }
 </style>

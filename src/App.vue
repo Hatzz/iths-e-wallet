0af1556c23e21b03ffee34c50bee81b3837af934
Hatzz <git@hatzz.io>
2021-01-28 11:14:32 +0100

    Add fade transition when switching views

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="wrapper">
-    <router-view/>
+    <transition name="fade" mode="out-in">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
@@ -8,5 +10,17 @@
 .wrapper {
   max-width: 25rem;
   margin: 0 auto;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0
 }
 </style>
